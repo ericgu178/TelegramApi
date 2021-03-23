52a5893285c40b09982c9a0b752e0a9eb863b93a
EricGU178 <1143055543@qq.com>
2021-03-23 11:47:52 +0800

    sendMediaGroup

--- lib/OpenPlatform/Message/Index.php
+++ lib/OpenPlatform/Message/Index.php
@@ -78,4 +78,50 @@ class Index
         $result = Request::requestGet($url,[],true,self::$proxy);
         return $result;
     }
+
+    /**
+     * https://core.telegram.org/method/messages.sendMediaGroup
+     * 
+     * 发送组合图片消息
+     * 数组InputMediaAudio，InputMediaDocument，InputMediaPhoto和InputMediaVideo
+     * $data = ['chat_id' =>  '@wechatGroupQrCode','media' =>  json_encode([
+     *      [
+     *          'type'  =>  'photo', video audio
+     *          'media' =>  'https://ww1.sinaimg.cn/large/6d28b716ly1goswssp7huj20s311cq87.jpg',
+     *          'caption'   =>  '微博'
+     *      ],
+     *      [
+     *          'type'  =>  'photo',
+     *          'media' =>  $messaggInfo,
+     *          'caption'   =>  'aaaa'
+     *      ],
+     *  ],256)];
+     *              $postContent = [
+     *          chat_id' => $GLOBALS['chatId'],
+     *          media' => json_encode([
+     *             ['type' => 'photo', 'media' => 'attach://file1.png' ],
+     *             ['type' => 'photo', 'media' => 'attach://file2.png' ],
+     *          ),
+     *          file1.png' => new CURLFile(realpath($filePath1)),
+     *          file2.png' => new CURLFile(realpath($filePath2)),
+     *           ];
+     *               post($url, $postContent);
+     * @param string $photo 小于5M的图片链接
+     * @param string $caption 介绍
+     * @return void
+     * @author EricGU178
+     */
+    public function sendMediaGroup(array $media)
+    {
+        $url = $this->base_url . '/sendMediaGroup';
+        if (!\is_array($media)) {
+            throw new \Exception('必须是一个数组');
+        }
+        $data = [
+            'chat_id'   =>  $this->chat_id,
+            'media'     =>  json_encode($media,256)
+        ];
+        $result = Request::requestPost($url,$data,[],true,self::$proxy);
+        return $result;
+    }
 }
--- tools/Request.php
+++ tools/Request.php
@@ -33,4 +33,39 @@ class Request
 
         return $response;
     }
+
+    /**
+     * post 请求
+     *
+     * @param string $url
+     * @param array $headers
+     * @param boolean $isProxy
+     * @param string $proxy
+     * @return void
+     * @author EricGU178
+     */
+    static public function requestPost(string $url,array $data = [], array $headers = [], $isProxy = false , $proxy = '')
+    {
+        $curl  = curl_init();
+        //设置选项，包括URL
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);//绕过ssl验证
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    
+        if ($isProxy == true) {
+            curl_setopt($curl, CURLOPT_PROXYTYPE, CURLPROXY_HTTP);
+            curl_setopt($curl, CURLOPT_PROXY, $proxy);
+        }
+        //执行并获取HTML文档内容
+        $response = curl_exec($curl);
+        //释放curl句柄
+        curl_close($curl);
+    
+        return $response;
+    }
 }
